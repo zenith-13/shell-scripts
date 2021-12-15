54f64097c5c792399135b1cc90d353dfd7a447a1
zenith-13 <zmccullough@ucsb.edu>
2021-12-15 14:26:42 -0500

    add code to deal values at random

--- for-loop.sh
+++ for-loop.sh
@@ -1,2 +1,13 @@
 # for-loop example
 # we will be creating a program that "deals" values at random
+#!/bin/bash
+cards=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10)
+
+echo "how many cards would you like?"
+read value
+
+for i in $(seq 1 $value);
+do echo ${cards[RANDOM%${cards[@]}]};
+
+done
+# `${cards[RANDOM%${#cards[@]}]}` comman randomly selects values for `cards`
